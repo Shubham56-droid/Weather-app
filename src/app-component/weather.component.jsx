acce3b3c938045308242eaddd1f4ef9398eeeb00
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-07-29 22:58:46 +0530

    Add files via upload

---
+++ src/app-component/weather.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Weather = () => {
+    return(
+        <div className="container">
+            <div className="cards">
+                <h1>London</h1>
+                <h5 className="py-4">
+                    
+                </h5>
+            </div>
+        </div>
+    );
+}
+
+export default Weather;
