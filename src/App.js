5748fac1b80eaaeb7bc120eb2be61e2d1760d413
Shubham Bawankar <71594948+Shubham56-droid@users.noreply.github.com>
2021-07-31 23:25:37 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react'
 import "./App.css";
-import "bootstrap/dist/css/bootstrap.min.css";
-import Weather from "./app_component/weather.component";
-
+import { Weather } from "./components/Weather";
 
 function App() {
   return (
     // we  have  to wrap it  in opening  closing  tags it  is  important
-    <div className="App">
-      <Weather/>
-    </div>
+  <>
+  <Weather/>
+  </>
   );
 }
 export default App;
